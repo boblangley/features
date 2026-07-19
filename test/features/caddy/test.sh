a141e6331f467d75936093c6f18194cfd37f35c6
Bob Langley <bob@wyrd.team>
2026-07-19 05:01:27 +0000

    test(caddy): tolerate formatted indentation

--- test/features/caddy/test.sh
+++ test/features/caddy/test.sh
@@ -8,7 +8,7 @@ source dev-container-features-test-lib
 check "caddy is installed" caddy version
 check "caddy service is registered" test -f /etc/s6-overlay/user-bundles.d/user/contents.d/caddy
 check "watcher service is registered" test -f /etc/s6-overlay/user-bundles.d/user/contents.d/caddy-config-watcher
-check "admin uses a Unix socket" grep -q '^    admin unix//run/caddy/admin.sock$' /etc/caddy/Caddyfile
+check "admin uses a Unix socket" grep -Eq '^[[:space:]]+admin unix//run/caddy/admin\.sock$' /etc/caddy/Caddyfile
 check "fragment wildcard is imported" grep -q '^import /etc/caddy/conf.d/\*.caddy$' /etc/caddy/Caddyfile
 check "fragment directory is setgid" test "$(stat -c %a /etc/caddy/conf.d)" = 2775
 check "remote user can manage fragments" bash -c 'id -nG vscode | tr " " "\n" | grep -qx caddy-config'
